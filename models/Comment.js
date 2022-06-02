673b17e1686200abf3e443ea2c9da51b48ef29e4
Jessie Doherty <zhuzhu930@yahoo.com>
2022-06-02 13:14:08 -0500

    modifying Comment Model

--- models/Comment.js
+++ models/Comment.js
@@ -1,15 +1,9 @@
 //writing Comment model for the database
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 //Define class Comment
-class Comment extends Model {
-  // No need to check password
-  // checkPassword(loginPw) {
-  //   return bcrypt.compareSync(loginPw, this.password);
-  // }
-}
+class Comment extends Model { }
 
 Comment.init(
   {
