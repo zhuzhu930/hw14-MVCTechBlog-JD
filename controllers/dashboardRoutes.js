f464798faeeee40d5059ec4e4885f5110ec7f4e1
Jessie Doherty <zhuzhu930@yahoo.com>
2022-06-16 05:09:33 -0500

    add a dashboardRoutes file

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Blog, User, Comment} = require('../models'); 
+const withAuth = require('../utils/auth'); 
+
+// Use withAuth middleware to prevent access to route
+router.get('/', withAuth, async (req, res) => 
+// {
+//     try {
+//       // Find the logged in user based on the session Id
+//       const userData = await User.findByPk(req.session.user_id, {
+//         //not showing password to protect user privacy
+//         attributes: { exclude: ['password']},
+//         include: [{ model: Blog }],
+//       });
+//       // Serialize user data
+//       const user = userData.get({ plain: true });
+  
+//       res.render('dashboard', {
+//         //destructuring user
+//         ...user, 
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err)
+//     }
+//   }
+{
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                //use ID from the session
+                user_id: req.session.user_id
+            }, 
+            include: [
+                {
+                    model: Comment, 
+                    include: {
+                        model: User, 
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User, 
+                    attributes: ['username']
+                }
+            ]
+        }); 
+
+        const blogs = blogData.map(blog => blog.get({ plain: true }));
+        res.render('dashboard', { 
+            blogs, 
+            logged_in: true 
+        }); 
+    } catch (err) {
+        res.status(500).json(err)
+    }    
+});
+
+module.exports = router;
